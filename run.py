cff2db88ba90ca51aa862f78dce0ee634d606a89
L0rdGabbers <>
2023-05-03 17:11:21 +0000

    Adds date validation function

--- run.py
+++ run.py
@@ -1,4 +1,5 @@
 import gspread
+import time
 from google.oauth2.service_account import Credentials
 
 SCOPE = [
@@ -18,10 +19,22 @@ def get_patient_data():
     """
     print("Collecting new patient data.")
     print("Please enter date of birth.")
-    print("Data should appear as follows MM/DD/YY")
-    print("Example: 01/23/20\n")
+    print("Data should appear as follows MM/DD/YYYY")
+    print("Example: 01/23/2020\n")
 
     dob_str = input("Enter DoB here: ")
-    print(f"Patient's Date of Birth is {dob_str}") 
+    validate_date(dob_str)
+    print(f"Patient's Date of Birth is {dob_str}")
+
+
+def validate_date(date):
+    """
+    Inside the try, confirms whether string values follow the mm/dd/yyyy format.
+    Raises ValueError, if string does not follow this format.
+    """
+    try:
+        valid_date = time.strptime(date, '%m/%d/%Y')
+    except ValueError as e:
+        print(f'Invalid date: {e}, please try again\n')
 
 get_patient_data()
